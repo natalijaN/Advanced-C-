d1574145c9698ec2e343b956ff08a97955ad28ee
natalijaN <45513903+natalijaN@users.noreply.github.com>
2019-05-22 08:23:37 +0200

    commit

---
+++ Class 4 - Homework/PcConfigurator/Classes/Extensions.cs
@@ -0,0 +1,22 @@
+﻿using CSharpAdvanced_Class4.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpAdvanced_Class4
+{
+    public static class Extensions
+    {
+        public static string WithCurrency(this double value, string currency)
+        {
+            return $"{value:0,0.00}{currency}";
+        }
+
+        public static string PriceWithCurrency(this IPrice priced, string currency)
+        {
+            return priced.GetTotalPrice().WithCurrency(currency);
+        }
+    }
+}
